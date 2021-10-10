c8bcea0472149bd3f80e6c1bd890a91a6c529c99
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-10-10 20:37:59 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.header__btn').on('click', function(){
+        $('.header__btn').toggleClass('header__btn--active')
+        $('.header__list').toggleClass('header__list--active')
+    })
 });
