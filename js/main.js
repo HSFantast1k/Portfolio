27f34a99aa5405a6a33e289efecd2279dfb13c0d
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-11-03 20:24:54 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
-$(function(){
-    $('.header__btn').on('click', function(){
+$(function () {
+    $('.header__btn').on('click', function () {
         $('.header__btn').toggleClass('header__btn--active')
         $('.header__list').toggleClass('header__list--active')
-    })
+    });
+
+
+
+    // Плавний скрол вверх по якорям
+    // Menu
+    $(document).ready(function(){
+        $(".header__list").on("click","a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+    
+            //забираем идентификатор бока с атрибута href
+            var id  = $(this).attr('href'),
+    
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;
+            
+            //анимируем переход на расстояние - top за 1500 мс
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    });
+    //Footer
+    $(".backTo__link").on("click", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
 });
